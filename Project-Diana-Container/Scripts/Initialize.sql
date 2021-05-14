3675a4509360c20c5d658fe7c024a0ac360182e3
Dennis D. Silva <dsilva609@gmail.com>
2021-05-14 00:08:40 -0400

    feat: update paths

--- Project-Diana-Container/Scripts/Initialize.sql
+++ Project-Diana-Container/Scripts/Initialize.sql
@@ -1,13 +1,14 @@
-:r 0001-DB-Create.sql
+:r Scripts/Databases/0001-DB-Create.sql
 
-:r 0001-AspNetRoles-Create.sql
-:r 0002-AspNetUserRoles-Create.sql
-:r 0003-AspNetUsers-Create.sql
-:r 0004-Albums-Create.sql
-:r 0005-Books-Create.sql
-:r 0006-Games-Create.sql
-:r 0007-Movies-Create.sql
-:r 0008-Pops-Create.sql
-:r 0009-Tracks-Create.sql
-:r 0010-Wishes-Create.sql
-:r 0011-RefreshTokens-Create.sql
+:r Scripts/Tables/0001-AspNetRoles-Create.sql
+:r Scripts/Tables/0002-AspNetUserRoles-Create.sql
+:r Scripts/Tables/0003-AspNetUsers-Create.sql
+:r Scripts/Tables/0004-Albums-Create.sql
+:r Scripts/Tables/0005-Books-Create.sql
+:r Scripts/Tables/0006-Games-Create.sql
+:r Scripts/Tables/0007-Movies-Create.sql
+:r Scripts/Tables/0008-Pops-Create.sql
+:r Scripts/Tables/0009-Tracks-Create.sql
+:r Scripts/Tables/0010-Wishes-Create.sql
+:r Scripts/Tables/0011-RefreshTokens-Create.sql
+ 
